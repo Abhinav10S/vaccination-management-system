19fdbb43fee6cbe03a917c9de756914b5a33d3e3
Abhinav <abhinavsrivastav687@gmail.com>
2023-06-09 13:49:36 +0530

    added

---
+++ vaccination-management-system/src/main/java/com/example/vaccinationmanagementsystem/VaccinationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaccinationmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationManagementSystemApplication.class, args);
+	}
+
+}
---
+++ vaccination-management-system/src/test/java/com/example/vaccinationmanagementsystem/VaccinationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaccinationmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
